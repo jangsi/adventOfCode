10695517af08858fe10907f1ff1b17896aa442f5
Jang Si <21132787+jangsi@users.noreply.github.com>
2022-12-04 13:29:03 -0500

    simplify round one

--- src/adventOfCodeDayTwo/main.go
+++ src/adventOfCodeDayTwo/main.go
@@ -64,49 +64,39 @@ func parseStrategy(strat func(them string, you string) int) (int, error) {
 	return total, nil
 }
 
+type game struct {
+	self string
+	beat string
+}
+
 // The winner of the whole tournament is the player with the highest score.
 // Your total score is the sum of your scores for each round.
 // The score for a single round is the score for the shape you selected
 // (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round
 // (0 if you lost, 3 if the round was a draw, and 6 if you won).
 func playRoundOne(them string, you string) int {
+	gameRound := game{
+		self: you,
+	}
 	var score int
 	switch codec[you] {
 	case Rock:
+		gameRound.beat = Scissors
 		score = RockScore
 	case Paper:
+		gameRound.beat = Rock
 		score = PaperScore
 	case Scissors:
+		gameRound.beat = Paper
 		score = ScissorsScore
 	}
 	if codec[them] == codec[you] {
 		return score + DrawScore
 	}
-	if codec[them] == Rock {
-		if codec[you] == Paper {
-			return score + WinScore
-		}
-		if codec[you] == Scissors {
-			return score + LostScore
-		}
+	if gameRound.beat == codec[them] {
+		return score + WinScore
 	}
-	if codec[them] == Paper {
-		if codec[you] == Scissors {
-			return score + WinScore
-		}
-		if codec[you] == Rock {
-			return score + LostScore
-		}
-	}
-	if codec[them] == Scissors {
-		if codec[you] == Rock {
-			return score + WinScore
-		}
-		if codec[you] == Paper {
-			return score + LostScore
-		}
-	}
-	return score
+	return score + LostScore
 }
 
 // "Anyway, the second column says how the round needs to end: X means you need to lose,
