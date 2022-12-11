3f8e5229b64ea20dc7c8bfd696a9e1e367799816
Jang Si <21132787+jangsi@users.noreply.github.com>
2022-12-11 14:30:23 -0500

    q3p2

--- src/adventOfCodeDayThree/main.go
+++ src/adventOfCodeDayThree/main.go
@@ -7,12 +7,19 @@ import (
 )
 
 func main() {
-	answerOne, err := sumPriorities()
+	answerOne, err := sumPriorities(basicPriorities)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("Advent day one part one: %d\n", answerOne)
+	fmt.Printf("Advent day three part one: %d\n", answerOne)
+
+	answerTwo, err := sumPriorities(findBadgeRunePriorities)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	fmt.Printf("Advent day three part two: %d\n", answerTwo)
 }
 
 func runeToPriority(r rune) int {
@@ -26,16 +33,14 @@ func runeToPriority(r rune) int {
 	return 0
 }
 
-func sumPriorities() (int, error) {
-	f, err := os.Open("input.txt")
-	if err != nil {
-		return 0, err
+func findBadgeRunePriorities(group []string) (int, []string) {
+	if len(group) < 3 {
+		return 0, group
 	}
-	input := bufio.NewScanner(f)
 	var corpus = make(map[rune]int)
+	var candidates = make(map[rune]int)
 	var priority int
-	for input.Scan() {
-		text := input.Text()
+	for _, text := range group {
 		halfway := len(text) / 2
 		h1 := text[:halfway]
 		h2 := text[halfway:]
@@ -43,12 +48,64 @@ func sumPriorities() (int, error) {
 			corpus[c] = 1
 		}
 		for _, c := range h2 {
-			if _, ok := corpus[c]; ok {
-				priority += runeToPriority(c)
-				delete(corpus, c)
+			if _, ok := corpus[c]; !ok {
+				corpus[c] = -1
+			} else {
+				if corpus[c] != -1 {
+					corpus[c] += 1
+				}
+			}
+		}
+		for r, occurence := range corpus {
+			if occurence == 1 || occurence == -1 {
+				candidates[r] -= 1
 			}
 		}
 		corpus = make(map[rune]int)
+	}
+	for r, occurence := range candidates {
+		if occurence == -3 {
+			priority += runeToPriority(r)
+		}
+	}
+	return priority, []string{}
+}
+
+func basicPriorities(group []string) (int, []string) {
+	text := group[0]
+	var corpus = make(map[rune]int)
+	var priority int
+	halfway := len(text) / 2
+	h1 := text[:halfway]
+	h2 := text[halfway:]
+	for _, c := range h1 {
+		corpus[c] = 1
+	}
+	for _, c := range h2 {
+		if _, ok := corpus[c]; ok {
+			priority += runeToPriority(c)
+			delete(corpus, c)
+		}
+	}
+	group = group[len(group):]
+	return priority, group
+}
+
+func sumPriorities(alg func(group []string) (int, []string)) (int, error) {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		return 0, err
+	}
+	input := bufio.NewScanner(f)
+
+	var priority int
+	group := []string{}
+	for input.Scan() {
+		text := input.Text()
+		group = append(group, text)
+		p, g := alg(group)
+		priority += p
+		group = g
 	}
 	f.Close()
 	return priority, nil
